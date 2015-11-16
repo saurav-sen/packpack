82c9715c8431f6824044951fa85ccef760e73074
sourabhnits <sourabhnits@gmail.com>
2015-11-16 23:44:45 +0530

    Added ServiceRegistry for spring bean.

--- services/src/main/java/com/pack/pack/services/PackRepositoryService.java
+++ services/src/main/java/com/pack/pack/services/PackRepositoryService.java
@@ -4,6 +4,7 @@ import org.ektorp.CouchDbConnector;
 import org.ektorp.support.CouchDbRepositorySupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.pack.pack.model.Pack;
@@ -14,11 +15,11 @@ import com.pack.pack.model.Pack;
  *
  */
 @Component
+@Scope("singleton")
 public class PackRepositoryService extends CouchDbRepositorySupport<Pack>{
 
 	@Autowired
 	public PackRepositoryService(@Qualifier("packpackDB") CouchDbConnector db) {
 		super(Pack.class, db);
 	}
-
 }
--- services/src/main/java/com/pack/pack/services/registry/ServiceRegistry.java
+++ services/src/main/java/com/pack/pack/services/registry/ServiceRegistry.java
@@ -1,5 +1,11 @@
 package com.pack.pack.services.registry;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * 
  * @author Saurav
@@ -9,6 +15,38 @@ public class ServiceRegistry {
 
 	public static final ServiceRegistry INSTANCE = new ServiceRegistry();
 	
+	private ApplicationContext appContext;
+	
+	private Lock lock = new ReentrantReadWriteLock().readLock();
+	
+	private boolean initializedServices = false;
+	
 	private ServiceRegistry() {
+	}
+	
+	public void init() {
+		if(lock.tryLock()) {
+			try {
+				if(!initializedServices) {
+					appContext = new ClassPathXmlApplicationContext("META-INF/services.xml");
+					if(appContext == null) {
+						throw new RuntimeException("Error initializing spring "
+								+ "context for repository services");
+					}
+					((ClassPathXmlApplicationContext)appContext).registerShutdownHook();
+					initializedServices = true;
+				}
+			} finally {
+				lock.unlock();
+			}
+		}
+	}
+	
+	public <T> T findService(Class<T> serviceClass) {
+		if(appContext == null) {
+			throw new RuntimeException("Error initializing spring "
+					+ "context for repository services");
+		}
+		return appContext.getBean(serviceClass);
 	}
 }
