dced1570d26160eaa6096b339ef9eb1f717ee2f4
sourabhnits <sourabhnits@gmail.com>
2015-11-17 00:07:21 +0530

    Added getAll(...) with pagination in PackRepositoryService.

--- services/src/main/java/com/pack/pack/services/PackRepositoryService.java
+++ services/src/main/java/com/pack/pack/services/PackRepositoryService.java
@@ -1,13 +1,20 @@
 package com.pack.pack.services;
 
+import java.util.List;
+
 import org.ektorp.CouchDbConnector;
+import org.ektorp.Page;
+import org.ektorp.PageRequest;
+import org.ektorp.ViewQuery;
 import org.ektorp.support.CouchDbRepositorySupport;
+import org.ektorp.support.GenerateView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.pack.pack.model.Pack;
+import com.pack.pack.model.PackComment;
 
 /**
  * 
@@ -21,5 +28,23 @@ public class PackRepositoryService extends CouchDbRepositorySupport<Pack>{
 	@Autowired
 	public PackRepositoryService(@Qualifier("packpackDB") CouchDbConnector db) {
 		super(Pack.class, db);
+	}
+	
+	public void addComment(PackComment comment) {
+		if(comment.getPackId() != null) {
+			db.create(comment);
+		}
+	}
+	
+	@Override
+	@GenerateView
+	public List<Pack> getAll() {
+		ViewQuery query = createQuery("all").descending(true).includeDocs(true);
+		return db.queryView(query, Pack.class);
+	}
+	
+	public Page<Pack> getAll(PageRequest page) {
+		ViewQuery query = createQuery("all").descending(true).includeDocs(true);
+		return db.queryForPage(query, page, Pack.class);
 	}
 }
