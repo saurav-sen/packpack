86dc1a39224da4c9578f3b58da8cea3391a5b476
sourabhnits <sourabhnits@gmail.com>
2015-11-23 21:33:09 +0530

    Added skeleton code for login api.

--- api/src/main/java/com/pack/pack/rest/api/UserResource.java
+++ api/src/main/java/com/pack/pack/rest/api/UserResource.java
@@ -1,7 +1,18 @@
 package com.pack.pack.rest.api;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.Provider;
+
+import com.pack.pack.model.web.JUser;
+import com.pack.pack.model.web.JUsers;
+import com.pack.pack.model.web.dto.UserDTO;
+import com.pack.pack.services.exception.PackPackException;
 
 /**
  * 
@@ -12,4 +23,35 @@ import javax.ws.rs.ext.Provider;
 @Path("/users")
 public class UserResource {
 
+	@GET
+	@Path("id/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public JUser getUserById(@PathParam("id") String id)
+			throws PackPackException {
+		return null;
+	}
+
+	@GET
+	@Path("username/{username}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public JUser getUserByUsername(@PathParam("username") String username)
+			throws PackPackException {
+		return null;
+	}
+
+	@GET
+	@Path("name/{namePattern}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public JUsers getUsersByName(@PathParam("namePattern") String namePattern)
+			throws PackPackException {
+		return null;
+	}
+	
+	@POST
+	@Path("login")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public JUser login(UserDTO loginDTO) throws PackPackException {
+		return null;
+	}
 }
---
+++ model/src/main/java/com/pack/pack/model/web/dto/UserDTO.java
@@ -0,0 +1,29 @@
+package com.pack.pack.model.web.dto;
+
+/**
+ * 
+ * @author Saurav
+ *
+ */
+public class UserDTO {
+
+	private String username;
+	
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
