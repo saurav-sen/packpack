6c4a7f06069e3f7fd3b0d63e9f28d760e0c37f8d
sourabhnits <sourabhnits@gmail.com>
2015-12-06 12:25:47 +0530

    Fixed build issue for api/ module.

---
+++ api/src/main/java/com/pack/pack/rest/api/slf4j/LogBridgeListener.java
@@ -0,0 +1,33 @@
+package com.pack.pack.rest.api.slf4j;
+
+import java.util.logging.Handler;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
+/**
+ * 
+ * @author Saurav
+ *
+ */
+public class LogBridgeListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        Logger logger = LogManager.getLogManager().getLogger("");
+        Handler[] handlers = logger.getHandlers();
+        for (Handler handler : handlers) {
+            logger.removeHandler(handler);
+        }
+        SLF4JBridgeHandler.install();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+
+}
