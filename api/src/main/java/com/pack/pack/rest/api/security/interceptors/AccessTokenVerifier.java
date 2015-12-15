92455d512778c82b8f5599620122ce002d735bcf
sourabhnits <sourabhnits@gmail.com>
2015-12-16 01:54:44 +0530

    Added skeleton method for request interceptor which will be responsible for validating access token in AUTHORIZATION header and thereby restricting access.

---
+++ api/src/main/java/com/pack/pack/rest/api/security/interceptors/AccessTokenVerifier.java
@@ -0,0 +1,21 @@
+package com.pack.pack.rest.api.security.interceptors;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+
+/**
+ * 
+ * @author Saurav
+ *
+ */
+public class AccessTokenVerifier implements ContainerRequestFilter {
+
+	@Override
+	public void filter(ContainerRequestContext requestContext)
+			throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+}
