58dfdb11a49e48541d8b669f45e18f5513568890
saurav-sen <sourabhnits@gmail.com>
2016-02-04 09:23:54 +0530

    Moved ServiceRegistry ininitialization and EvenentManager initialization to AppContextListener which seems to be the correct place instead of ResourceConfig.

--- api/src/main/java/com/pack/pack/rest/api/context/AppContextListener.java
+++ api/src/main/java/com/pack/pack/rest/api/context/AppContextListener.java
@@ -3,23 +3,36 @@ package com.pack.pack.rest.api.context;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.pack.pack.event.IEventListener;
 import com.pack.pack.event.MsgEvent;
+import com.pack.pack.message.listeners.EventListener;
 import com.pack.pack.services.registry.EventManager;
+import com.pack.pack.services.registry.ServiceRegistry;
 
 /**
  * 
  * @author Saurav
  *
  */
-public class AppContextListener implements ServletContextListener, IEventListener{
+public class AppContextListener implements ServletContextListener, IEventListener {
+	
+	private static Logger logger = LoggerFactory.getLogger(AppContextListener.class);
 
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		try {
-			EventManager.INSTANCE.registerListener(this);
+			logger.info("Starting service-registry");
+			ServiceRegistry.INSTANCE.init();
+			logger.info("Started service-registry, successfully");
+			logger.info("Starting event-manager & registering generic listener");
+			EventManager.INSTANCE.registerListener(new EventListener());
+			logger.info("Started event-manager & registered generic listener, successfully");
 		} catch (InterruptedException e) {
-			e.printStackTrace();
+			logger.error("Failed to start application: " + e.getMessage(), e);
+			throw new RuntimeException(e.getMessage(), e);
 		}
 	}
 
--- api/src/main/java/com/pack/pack/rest/api/ws/app/PackPackWSApp.java
+++ api/src/main/java/com/pack/pack/rest/api/ws/app/PackPackWSApp.java
@@ -1,12 +1,6 @@
 package com.pack.pack.rest.api.ws.app;
 
 import org.glassfish.jersey.server.ResourceConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.pack.pack.message.listeners.EventListener;
-import com.pack.pack.services.registry.EventManager;
-import com.pack.pack.services.registry.ServiceRegistry;
 
 /**
  * 
@@ -15,18 +9,7 @@ import com.pack.pack.services.registry.ServiceRegistry;
  */
 public class PackPackWSApp extends ResourceConfig {
 	
-	private static Logger logger = LoggerFactory.getLogger(PackPackWSApp.class);
-
 	public PackPackWSApp() {
-		logger.info("Starting application");
 		packages(true, "com.pack.pack.rest.api");
-		try {
-			ServiceRegistry.INSTANCE.init();
-			EventManager.INSTANCE.registerListener(new EventListener());
-			logger.info("Application initialized successfully");
-		} catch (Exception e) {
-			logger.error("Failed to start application: " + e.getMessage(), e);
-			throw new RuntimeException(e.getMessage(), e);
-		}
 	}
 }
