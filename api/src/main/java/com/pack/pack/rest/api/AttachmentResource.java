eda5f53a451dbf0db00d2ab198948ba30011d322
saurav-sen <sourabhnits@gmail.com>
2016-03-09 08:49:57 +0530

    Fixed Attachment GET endpoint handler functions.

--- api/src/main/java/com/pack/pack/rest/api/AttachmentResource.java
+++ api/src/main/java/com/pack/pack/rest/api/AttachmentResource.java
@@ -2,11 +2,9 @@ package com.pack.pack.rest.api;
 
 import static com.pack.pack.util.SystemPropertyUtil.ATTACHMENT;
 import static com.pack.pack.util.SystemPropertyUtil.IMAGE;
-import static com.pack.pack.util.SystemPropertyUtil.IMAGE_ATTACHMENT_URL_SUFFIX;
 import static com.pack.pack.util.SystemPropertyUtil.PROFILE;
 import static com.pack.pack.util.SystemPropertyUtil.URL_SEPARATOR;
 import static com.pack.pack.util.SystemPropertyUtil.VIDEO;
-import static com.pack.pack.util.SystemPropertyUtil.VIDEO_ATTACHMENT_URL_SUFFIX;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -18,10 +16,8 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.Provider;
 
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
@@ -45,17 +41,21 @@ import com.pack.pack.util.SystemPropertyUtil;
 @Provider
 @Path("/" + ATTACHMENT)
 public class AttachmentResource {
-	
-	private static Logger logger = LoggerFactory.getLogger(AttachmentResource.class);
-	
+
+	private static Logger logger = LoggerFactory
+			.getLogger(AttachmentResource.class);
+
 	@GET
-	@Path(PROFILE + URL_SEPARATOR + IMAGE + URL_SEPARATOR + "{userId}" + URL_SEPARATOR + "{fileName}")
-	@Produces({"image/png", "image/jpg"})
-	public Response getProfilePictureImage(@PathParam("userId") String userId, @PathParam("fileName") String fileName) throws PackPackException {
+	@Path(PROFILE + URL_SEPARATOR + IMAGE + URL_SEPARATOR + "{userId}"
+			+ URL_SEPARATOR + "{fileName}")
+	@Produces({ "image/png", "image/jpg" })
+	public Response getProfilePictureImage(@PathParam("userId") String userId,
+			@PathParam("fileName") String fileName) throws PackPackException {
 		try {
-			String profilePictureHome = SystemPropertyUtil.getProfilePictureHome();
+			String profilePictureHome = SystemPropertyUtil
+					.getProfilePictureHome();
 			StringBuilder path = new StringBuilder(profilePictureHome);
-			if(!profilePictureHome.endsWith(File.separator)) {
+			if (!profilePictureHome.endsWith(File.separator)) {
 				path = path.append(File.separator);
 			}
 			path.append(userId);
@@ -70,43 +70,57 @@ public class AttachmentResource {
 	}
 
 	@GET
-	@Path(IMAGE)
-	@Produces({"image/png", "image/jpg"})
-	public Response getImageAttachment(@Context UriInfo uriInfo) throws PackPackException {
+	@Path(IMAGE + URL_SEPARATOR + "{topicId}" + URL_SEPARATOR + "{packId}"
+			+ URL_SEPARATOR + "{fileName}")
+	@Produces({ "image/png", "image/jpg" })
+	public Response getImageAttachment(@PathParam("topicId") String topicId,
+			@PathParam("packId") String packId,
+			@PathParam("fileName") String fileName) throws PackPackException {
 		try {
 			String imageHome = SystemPropertyUtil.getImageHome();
-			String path = uriInfo.getPath();
-			int index = path.indexOf(IMAGE_ATTACHMENT_URL_SUFFIX) + IMAGE_ATTACHMENT_URL_SUFFIX.length();
-			path = path.substring(index);
-			path = imageHome + path;
-			path = path.replaceAll(URL_SEPARATOR, File.separator);
-			File imageFile = new File(path);
+			StringBuilder path = new StringBuilder(imageHome);
+			if (!imageHome.endsWith(File.separator)) {
+				path.append(File.separator);
+			}
+			path.append(topicId);
+			path.append(File.separator);
+			path.append(packId);
+			path.append(File.separator);
+			path.append(fileName);
+			File imageFile = new File(path.toString());
 			return ImageUtil.buildResponse(imageFile);
 		} catch (FileNotFoundException e) {
 			logger.info(e.getMessage(), e);
 			throw new PackPackException("TODO", e.getMessage(), e);
 		}
 	}
-	
+
 	@GET
-	@Path(VIDEO)
-	//@Produces({"image/png", "image/jpg"})
-	public Response getVideoAttachment(@Context UriInfo uriInfo) throws PackPackException {
+	@Path(VIDEO + URL_SEPARATOR + "{topicId}" + URL_SEPARATOR + "{packId}"
+			+ URL_SEPARATOR + "{fileName}")
+	// @Produces({"image/png", "image/jpg"})
+	public Response getVideoAttachment(@PathParam("topicId") String topicId,
+			@PathParam("packId") String packId,
+			@PathParam("fileName") String fileName) throws PackPackException {
 		try {
 			String videoHome = SystemPropertyUtil.getVideoHome();
-			String path = uriInfo.getPath();
-			int index = path.indexOf(VIDEO_ATTACHMENT_URL_SUFFIX) + VIDEO_ATTACHMENT_URL_SUFFIX.length();
-			path = path.substring(index);
-			path = videoHome + path;
-			path = path.replaceAll(URL_SEPARATOR, File.separator);
-			File videoFile = new File(path);
+			StringBuilder path = new StringBuilder(videoHome);
+			if (!videoHome.endsWith(File.separator)) {
+				path.append(File.separator);
+			}
+			path.append(topicId);
+			path.append(File.separator);
+			path.append(packId);
+			path.append(File.separator);
+			path.append(fileName);
+			File videoFile = new File(path.toString());
 			return ImageUtil.buildResponse(videoFile);
 		} catch (FileNotFoundException e) {
-			//logger.info(e.getMessage(), e);
+			// logger.info(e.getMessage(), e);
 			throw new PackPackException("TODO", e.getMessage(), e);
 		}
 	}
-	
+
 	@PUT
 	@Path("image/topic/{topicId}/usr/{userId}")
 	@Consumes(value = MediaType.MULTIPART_FORM_DATA)
@@ -121,11 +135,12 @@ public class AttachmentResource {
 		String fileName = aboutFile.getFileName();
 		IPackService service = ServiceRegistry.INSTANCE
 				.findCompositeService(IPackService.class);
-		return service.uploadPack(file, fileName, title, description,
-				story, topicId, userId, null, PackAttachmentType.IMAGE, true);
+		return service.uploadPack(file, fileName, title, description, story,
+				topicId, userId, null, PackAttachmentType.IMAGE, true);
 	}
-	//http://javapapers.com/android/android-get-address-with-street-name-city-for-location-with-geocoding/
-	
+
+	// http://javapapers.com/android/android-get-address-with-street-name-city-for-location-with-geocoding/
+
 	@PUT
 	@Path("images/topic/{topicId}/pack/{packId}/usr/{userId}")
 	@Consumes(value = MediaType.MULTIPART_FORM_DATA)
@@ -140,11 +155,11 @@ public class AttachmentResource {
 		return service.updatePack(file, aboutFile.getFileName(),
 				PackAttachmentType.IMAGE, packId, topicId, userId);
 	}
-	
+
 	@PUT
 	@Path("video/topic/{topicId}/usr/{userId}")
 	@Consumes(value = MediaType.MULTIPART_FORM_DATA)
-	@Produces(value =MediaType.APPLICATION_JSON)
+	@Produces(value = MediaType.APPLICATION_JSON)
 	public JPack uploadVideoPack(@FormDataParam("file") InputStream file,
 			@FormDataParam("file") FormDataContentDisposition aboutFile,
 			@FormDataParam("title") String title,
@@ -155,10 +170,10 @@ public class AttachmentResource {
 		String fileName = aboutFile.getFileName();
 		IPackService service = ServiceRegistry.INSTANCE
 				.findCompositeService(IPackService.class);
-		return service.uploadPack(file, fileName, title, description,
-				story, topicId, userId, null, PackAttachmentType.IMAGE, true);
+		return service.uploadPack(file, fileName, title, description, story,
+				topicId, userId, null, PackAttachmentType.IMAGE, true);
 	}
-	
+
 	@PUT
 	@Path("video/topic/{topicId}/pack/{packId}/usr/{userId}")
 	@Consumes(value = MediaType.MULTIPART_FORM_DATA)
