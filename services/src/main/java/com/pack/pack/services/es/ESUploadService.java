a9d410622a66fbbde5eff8ecdcd8de01d11667f5
saurav-sen <sourabhnits@gmail.com>
2016-03-30 01:26:50 +0530

    Modified while(...) condition of batch upload logic for uploading user-details of newly registered user to Elasticsearch data-store.

--- services/src/main/java/com/pack/pack/services/es/ESUploadService.java
+++ services/src/main/java/com/pack/pack/services/es/ESUploadService.java
@@ -100,7 +100,7 @@ public class ESUploadService {
 		@Override
 		public void run() {
 			try {
-				while (true) {
+				while (!stopSignal) {
 					User newUser = producerQueue.take();
 					uploadNewUserDetails(newUser);
 				}
